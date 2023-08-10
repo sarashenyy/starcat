516bc85f204d7c3527fdaeaf859951d0cfe24bbe
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-08-10 16:27:34 +0800

    add `meow`

---
+++ starcat/bonus.py
@@ -0,0 +1,6 @@
+def meow():
+    # print(" /\\_/\\")
+    # print("( o.o )")
+    # print(" > ω <")
+    print("ฅ^•ﻌ•^ฅ Meow there! I'm STARCAT, here to help you with star cluster analysis, meow ~")
+    return
