9860991a7d8e1aaeb5b0634be377c3c397cd54fe
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-08-10 22:39:12 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='starcat',
     version='0.0.0',
@@ -9,13 +15,11 @@ setup(
     author='Yueyue Shen',
     author_email='shenyy@bao.ac.cn',
     description='Star cluster analysis toolkit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_dir={'starcat': 'starcat'},
     # include_package_data=True,
     package_data={'': ['LICENSE', 'README.md'],
-                  'starcat': ['data/*']}
-    # install_requires=['sphinx',
-    #                   'numpy',
-    #                   'scipy',
-    #                   'matplotlib',
-    #                   'astropy'],
+                  'starcat': ['data/*']},
+    install_requires=requirements
 )
