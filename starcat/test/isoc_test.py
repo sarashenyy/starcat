ea9b88745debff1d65b523afd9970d1f81b15981
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-09-07 23:01:58 +0800

    updt starcat/test

--- starcat/test/isoc_test.py
+++ starcat/test/isoc_test.py
@@ -17,18 +17,18 @@ def draw_isoc(isoc, model, photsyn):
 
     """
 
-    color_list = ["grey", "green", "orange", "red", "blue", "skyblue", "pink", "purple", "grey", "black"]
+    color_list = ['grey', 'green', 'orange', 'red', 'blue', 'skyblue', 'pink', 'purple', 'grey', 'black']
     source = config.config[model][photsyn]
-    phase = source["phase"]
+    phase = source['phase']
 
     fig, ax = plt.subplots(figsize=(5, 6))
     for i, element in enumerate(phase):
-        if photsyn == "gaiaDR2":
-            isoc["color"] = isoc["G_BPmag"] - isoc["G_RPmag"]
-            ax.plot(isoc[isoc["phase"] == element]["color"], isoc[isoc["phase"] == element]["Gmag"],
+        if photsyn == 'gaiaDR2' or photsyn == 'gaiaEDR3':
+            isoc['color'] = isoc['G_BPmag'] - isoc['G_RPmag']
+            ax.plot(isoc[isoc['phase'] == element]['color'], isoc[isoc['phase'] == element]['Gmag'],
                     color=color_list[i], label=element)
-            ax.set_xlabel("BP-RP (mag)")
-            ax.set_ylabel("G (mag)")
+            ax.set_xlabel('BP-RP (mag)')
+            ax.set_ylabel('G (mag)')
     ax.legend()
     ax.invert_yaxis()
     fig.show()
