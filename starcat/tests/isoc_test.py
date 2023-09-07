e6e8e6ecfdbdc078c5573cbb5baf03673b986de9
sarashenyueyue <syy20000503@icloud.com>
2023-09-07 13:50:13 +0800

    add draw_isoc() in isoc_test.py & add isoc.ipynb

---
+++ starcat/tests/isoc_test.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+from .. import config
+
+
+def draw_isoc(isoc, model, photsyn):
+    """
+    Draw isochrone and color the stellar period.
+
+    Parameters
+    ----------
+    isoc : pd.DataFrame
+    model : str
+    photsyn : str
+
+    Returns
+    -------
+
+    """
+
+    color_list = ["grey", "green", "orange", "red", "blue", "skyblue", "pink", "purple", "grey", "black"]
+    source = config.config[model][photsyn]
+    phase = source["phase"]
+
+    fig, ax = plt.subplots(figsize=(5, 6))
+    for i, element in enumerate(phase):
+        if photsyn == "gaiaDR2":
+            isoc["color"] = isoc["G_BPmag"] - isoc["G_RPmag"]
+            ax.plot(isoc[isoc["phase"] == element]["color"], isoc[isoc["phase"] == element]["Gmag"],
+                    color=color_list[i], label=element)
+            ax.set_xlabel("BP-RP (mag)")
+            ax.set_ylabel("G (mag)")
+    ax.legend()
+    ax.invert_yaxis()
+    fig.show()
