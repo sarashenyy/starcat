9a3b752808acfd3f3fdcd3097c2a78837f1f4778
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-09-07 23:09:49 +0800

    add bulk_load in isoc.py & create bulkload_isocs.py

---
+++ script/bulkload_isocs.py
@@ -0,0 +1,13 @@
+from starcat import Isoc, Parsec
+
+# use Parsec model
+p = Parsec()
+i = Isoc(p)
+
+# bulk load isochrones
+logage_grid = (6.6, 10, 0.01)
+mh_grid = (-0.9, 0.7, 0.01)
+i.bulk_load(
+    photsyn='gaiaEDR3',
+    logage_grid=logage_grid, mh_grid=mh_grid
+)
--- starcat/isoc.py
+++ starcat/isoc.py
@@ -1,11 +1,13 @@
 import os
 from abc import ABC, abstractmethod
+
 import joblib
 import numpy as np
 from berliner import CMD
+from joblib import Parallel, delayed
 
-from .widgets import round_to_step
 from . import config
+from .widgets import round_to_step
 
 
 class Model(ABC):
@@ -34,6 +36,20 @@ class Model(ABC):
         """
         pass
 
+    def bulk_load(self, photsyn, n_jobs, **kwargs):
+        """
+        Bulk Laod isochrones.
+
+        Parameters
+        ----------
+        n_jobs
+        photsyn
+        kwargs : dict
+            - logage_grid (tuple): start, end, step
+            - mh_grid (tuple): start, end, step
+        """
+        pass
+
 
 class Parsec(Model):
     """
@@ -41,7 +57,7 @@ class Parsec(Model):
     """
 
     def __init__(self):
-        self.model = "parsec"
+        self.model = 'parsec'
 
     def get_isoc(self, photsyn, **kwargs):
         """
@@ -60,18 +76,18 @@ class Parsec(Model):
         -------
         pd.DataFrame: isochrone containing the evolutionary phase, initial mass and photometry bands.
         """
-        logage = round_to_step(kwargs.get("logage"), step=kwargs.get("logage_step"))
-        mh = round_to_step(kwargs.get("mh"), step=kwargs.get("mh_step"))
+        logage = round_to_step(kwargs.get('logage'), step=kwargs.get('logage_step'))
+        mh = round_to_step(kwargs.get('mh'), step=kwargs.get('mh_step'))
         # TODO: dm 用于确定最小质量 mass_min, 是否需要更改确定最小质量(最大光度)的方式？
         # dm = kwargs.get("dm")
         # mag_max = source["mag_max"]
         source = config.config[self.model][photsyn]
-        bands = source["bands"]
-        mini = source["mini"]
-        label = source["label"]
-        phase = source["phase"]
-        isoc_dir = source["isoc_dir"]
-        isoc_path = config.data_dir + isoc_dir + f"age{logage:+.2f}_mh{mh:+.2f}.joblib"
+        bands = source['bands']
+        mini = source['mini']
+        label = source['label']
+        phase = source['phase']
+        isoc_dir = source['isoc_dir']
+        isoc_path = config.data_dir + isoc_dir + f'age{logage:+.2f}_mh{mh:+.2f}.joblib'
 
         if os.path.exists(isoc_path):
             isochrone = joblib.load(isoc_path)
@@ -83,11 +99,11 @@ class Parsec(Model):
             # truncate isochrone, PMS~EAGB
             # ATTENTION! parsec use "label" to represent evolutionary phase, different from MIST("phase")
             isochrone = isochrone[
-                (isochrone["label"] >= min(label)) & (isochrone["label"] <= max(label))].to_pandas()
+                (isochrone['label'] >= min(label)) & (isochrone['label'] <= max(label))].to_pandas()
             # add evolutionary phase info
             for i, element in enumerate(label):
-                index = np.where(isochrone["label"] == element)[0]
-                isochrone.loc[index, "phase"] = phase[i]
+                index = np.where(isochrone['label'] == element)[0]
+                isochrone.loc[index, 'phase'] = phase[i]
         # TODO: 将以下两行定义质量范围的代码和上述一行定义dm的代码移出Isoc类之外
         # mass_min = min(isochrone[(isochrone[bands[0]] + dm) <= mag_max][mini])
         # mass_max = max(isochrone[mini])
@@ -95,9 +111,39 @@ class Parsec(Model):
         # save isochrone file
         if not os.path.exists(isoc_path):
             joblib.dump(isochrone, isoc_path)
-        useful_columns = ["phase", mini] + bands
+        useful_columns = ['phase', mini] + bands
         isoc = isochrone[useful_columns]
         return isoc
+
+    def bulk_load(self, photsyn, n_jobs, **kwargs):
+        """
+        Bulk Laod isochrones.
+
+        Parameters
+        ----------
+        n_jobs
+        photsyn
+        kwargs : dict
+            - logage_grid (tuple): start, end, step
+            - mh_grid (tuple): start, end, step
+        """
+        astart, aend, astep = kwargs.get('logage_grid')
+        mstart, mend, mstep = kwargs.get('mh_grid')
+        abin = np.arange(astart, aend, astep)
+        mbin = np.arange(mstart, mend, mstep)
+        logage_mh = []
+        for a in abin:
+            for m in mbin:
+                logage_mh.append([a, m])
+
+        # nested function, access variable in parent function
+        def bulk_load_wrapper(logage, mh):
+            self.get_isoc(photsyn=photsyn, logage=logage, mh=mh, logage_step=astep, mh_step=mstep)
+
+        # parallel excution
+        Parallel(n_jobs=n_jobs)(
+            delayed(bulk_load_wrapper)(logage, mh) for logage, mh in logage_mh
+        )
 
 
 class MIST(Model):
@@ -106,9 +152,23 @@ class MIST(Model):
     """
 
     def __init__(self):
-        self.model = "mist"
+        self.model = 'mist'
 
     def get_isoc(self, photsyn, **kwargs):
+        pass
+
+    def bulk_load(self, photsyn, n_jobs, **kwargs):
+        """
+        Bulk Laod isochrones.
+
+        Parameters
+        ----------
+        n_jobs
+        photsyn
+        kwargs : dict
+            - logage_grid (tuple): start, end, step
+            - mh_grid (tuple): start, end, step
+        """
         pass
 
 
@@ -149,3 +209,18 @@ class Isoc(object):
         ```
         """
         return self.model.get_isoc(photsyn=photsyn, **kwargs)
+
+    def bulk_load(self, photsyn: str, n_jobs: int = 20, **kwargs: dict) -> object:
+        """
+        Bulk Laod isochrones.
+
+        Parameters
+        ----------
+        n_jobs : int, optional
+            Default 20.
+        photsyn
+        kwargs : dict
+            - logage_grid (tuple): start, end, step
+            - mh_grid (tuple): start, end, step
+        """
+        self.model.bulk_load(photsyn=photsyn, n_jobs=n_jobs, **kwargs)
