0683b3bbd2045b5ddf6b13dd9c10c80606f43920
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-09-10 18:26:44 +0800

    updt

--- starcat/widgets.py
+++ starcat/widgets.py
@@ -1,22 +1,6 @@
-import logging
-import time
-from functools import wraps
-
 import numpy as np
 
 
 def round_to_step(arr, step):
     return np.round(arr / step) * step
 
-
-def log_time(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        run_time = end_time - start_time
-        logging.info(f"time init() : {run_time:.4f} s")
-        return result
-
-    return wrapper
