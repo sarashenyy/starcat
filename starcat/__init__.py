eb467d167c60d26987f748931100d9ac977705f6
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-09-11 21:43:22 +0800

    add config

--- starcat/__init__.py
+++ starcat/__init__.py
@@ -1,5 +1,6 @@
 from .binary import BinMS, BinSimple
 from .bonus import meow
+from .config import config
 from .imf import IMF
 from .isoc import Isoc, Parsec, MIST
 from .likelihood import Hist2Hist, Hist2Point, lnlike_4p, lnlike_2p
