5c5a795dd1987c43eba893638906c3fea2d542db
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-10-11 10:38:33 +0800

    update for package_data

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import glob
+import os
+
 from setuptools import setup, find_packages
+
+data_files = [os.path.relpath(file, 'starcat') for file in glob.glob('starcat/data/**', recursive=True)]
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,13 +19,15 @@ setup(
     url='https://github.com/sarashenyy/starcat',
     license='MIT',
     author='Yueyue Shen',
-    author_email='shenyy@bao.ac.cn',
+    author_email='shenyy@nao.cas.cn',
     description='Star cluster analysis toolkit',
     long_description=long_description,
     long_description_content_type="text/markdown",
     package_dir={'starcat': 'starcat'},
     # include_package_data=True,
+    # package_data={'': ['LICENSE', 'README.md'],
+    #               'starcat': ['data/*']},
     package_data={'': ['LICENSE', 'README.md'],
-                  'starcat': ['data/*']},
+                  'starcat': data_files},
     install_requires=requirements
 )
