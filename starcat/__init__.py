8df4c17174e6974f30c705167f07bca2e9097831
sarashenyy <shenyueyue22@mails.ucas.edu.cn>
2023-10-12 10:21:21 +0800

    updt

--- starcat/__init__.py
+++ starcat/__init__.py
@@ -1,7 +1,8 @@
-from .binary import BinMS, BinSimple
+from .binary import BinMS, BinSimple, BinMRD
 from .bonus import meow
 from .imf import IMF
 from .isoc import Isoc, Parsec, MIST
 from .likelihood import Hist2Hist, Hist2Point, lnlike_4p, lnlike_2p
 from .magerr import MagError
-from .synstars import SynStars, GaiaEDR3
+from .photerr import Photerr, GaiaEDR3, CSSTsim
+from .synstars import SynStars
